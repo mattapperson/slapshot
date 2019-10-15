57bce566294532e3e722c4f143ccec7acde102ca
Matt <me@mattapperson.com>
2019-10-15 01:18:28 +0000

    remove shouldUpdateSnapshot

--- src/call_when_online.ts
+++ src/call_when_online.ts
@@ -1,7 +1,16 @@
-import { runInOnlineMode, shouldUpdateSnapshot } from "./utils";
+import { runInOnlineMode } from "./utils";
 
 export const callWhenOnline = async (block: () => any) => {
-  if ((shouldUpdateSnapshot() && runInOnlineMode()) || runInOnlineMode()) {
+  let jestContext: any;
+  // @ts-ignore
+  expect().__slapshot__hack__context(context => (jestContext = context));
+
+  if (
+    (process.env.SHOULD_UPDATE_SNAPSHOTS === undefined &&
+      jestContext.shouldUpdateSnapshot) ||
+    (process.env.SHOULD_UPDATE_SNAPSHOTS === "true" && runInOnlineMode()) ||
+    runInOnlineMode()
+  ) {
     await block();
   }
 };
